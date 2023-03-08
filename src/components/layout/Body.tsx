f5939e6d9a0d659dbfd519860ba429995221b127
bo kyung <bkyungkeem@daum.net>
2023-03-08 18:01:45 +0900

    dd

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Body = () => {
     return (
-    <>
-        
-    </>
+        <>
+            <main className="ant-layout-content css-dev-only-do-not-override-1me4733">
+                <div className="site-layout-content">
+                    <div className="inner-content">
+                    </div>
+                </div>
+            </main>
+        </>
     );
 }
 
